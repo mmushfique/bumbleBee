ae20439f4accb3bb2973737a0fb89b20acf149f0
UMUSHMO <mohamed.mushfique@pearson.com>
2023-04-02 14:39:05 +0530

    fixed issues

--- src/main/java/com/mush/bumblebee/util/PasswordEncryption.java
+++ src/main/java/com/mush/bumblebee/util/PasswordEncryption.java
@@ -1,2 +1,23 @@
-package com.mush.bumblebee.util;public class PasswordEncryption {
+package com.mush.bumblebee.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class PasswordEncryption {
+
+    public static String encryptPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(password.getBytes());
+            byte[] bytes = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
