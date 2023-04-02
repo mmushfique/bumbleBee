bccf5aa8309d6f189a24d25aba01cf302e0d3291
UMUSHMO <mohamed.mushfique@pearson.com>
2023-04-02 14:31:33 +0530

    fixed issues

--- src/main/java/com/mush/bumblebee/injector/Injector.java
+++ src/main/java/com/mush/bumblebee/injector/Injector.java
@@ -1,12 +1,10 @@
 package com.mush.bumblebee.injector;
 
 import com.mush.bumblebee.dao.AdminManager;
-import com.mush.bumblebee.dao.ProductManager;
-import com.mush.bumblebee.service.AdminServiceImpl;
-import com.mush.bumblebee.service.ProductServiceImpl;
 
 public class Injector {
     public AdminManager getAdminManager(){
         return new AdminManager();
     }
+
 }
